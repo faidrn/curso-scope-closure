85cf0e758afb623bf18eb7e7261e72bc11ef6849
Fredy Izquierdo <m3talmaniaco@gmail.com>
2022-08-01 23:46:29 -0500

    Finish

---
+++ src/closure/mymoneybox.js
@@ -0,0 +1,24 @@
+function moneyBox(){
+    let saveCoins = 0;
+
+    function countCoins(coins){
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    }
+
+    return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(15);
+
+// 25
+
+const moneyBoxAna = moneyBox();
+moneyBoxAna(10);
+moneyBoxAna(20);
+moneyBoxAna(5);
+
+// 35
